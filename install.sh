9fbb084a51f62963856be2e8c0cf0e781f209a8c
Gareth Hall <gareth@communica.co.nz>
2022-06-14 22:35:33 +1200

    wip

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ installCommon ()
 {
     message "Common"
     sudo apt install \
+        ruby-full
         tree \
         -y
 }
@@ -14,9 +15,9 @@ addPPAs ()
     sudo add-apt-repository ppa:ondrej/php -y
 }
 
-installPHP ()
+installPHP7 ()
 {
-	message $1
+	message "PHP $1"
     sudo apt install \
         php$1-bcmath \
         php$1-common \
@@ -36,6 +37,27 @@ installPHP ()
         -y
 }
 
+installPHP8 ()
+{
+	message "PHP $1"
+    sudo apt install \
+        php8.0-bcmath \
+        php8.0-common \
+        php8.0-curl \
+        php8.0-dev \
+        php8.0-gd \
+        php8.0-imagick \
+        php8.0-mbstring \
+        php8.0-mongodb \
+        php8.0-mysql \
+        php8.0-pdo \
+        php8.0-redis \
+        php8.0-sqlite3 \
+        php8.0-xml \
+        php8.0-zip \
+        -y
+}
+
 message ()
 {
     echo "####################################"
@@ -43,12 +65,33 @@ message ()
     echo "####################################"
 }
 
-# sudo apt update
-
-# sudo apt upgrade
-
+sudo apt update -y
+udo apt upgrade -y
 
 installCommon
 # addPPAs
-installPHP "7.3"
-installPHP "7.4"
+installPHP7 "7.3"
+installPHP7 "7.4"
+installPHP8 "8.0"
+# installPHP8 "8.1"
+
+
+# Add to .zshrc
+# function switchPHP7.2() {
+#     sudo update-alternatives --set php /usr/bin/php7.2
+# }
+# function switchPHP7.3() {
+#     sudo update-alternatives --set php /usr/bin/php7.3
+# }
+# function switchPHP7.4() {
+#     sudo update-alternatives --set php /usr/bin/php7.4
+# }
+
+# function switchPHP8.0() {
+#     sudo update-alternatives --set php /usr/bin/php8.0
+# }
+
+# function lagoonToken() {
+#     ssh lagoon@lagoon -t token | sed 's/.\{1\}$//' | xclip -r -selection clipboard 
+#     echo 'Token copied to clipboard'
+# }
